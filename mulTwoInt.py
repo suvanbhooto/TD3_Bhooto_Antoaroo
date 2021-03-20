a6ca112c5470c97cc7965a947ae6f19083552d09
mushtaaq <mmantoaroo@student.udm.ac.mu>
2021-03-21 01:28:58 +0400

    [mulTwoInt.py]troisieme modification

--- mulTwoInt.py
+++ mulTwoInt.py
@@ -10,11 +10,24 @@ def main():
 	i=(len(sys.argv)-1)
 	print(i)
 	
-	if (i==2):
-		x = int(sys.argv[1])
-		y = int(sys.argv[2])
-		print (mul_num(x,y))
-	else :
-		print("error : only two argumentsare allowed")
-	
+	if (i==0):
+		n1=int(input("inserer le premier argument : "))
+		n2=int(input("inserer le deuxieme argument : "))
+		x = int (n1)
+		y = int (n2)
+		print(mul_num(x,y))
+	elif (i==1):
+		n1=int(input("inserer le deuxieme argument : "))
+		x = int (sys.argv[1])
+		y = int (n1)
+		print(mul_num(x,y))
+	elif (i==2):
+		x = int (sys.argv[1])
+		y = int (sys.argv[2])
+		print(mul_num(x,y))
+	else:
+		print("erreur : veuillez insererque deux arguements")
 main()
+		
+		
+
