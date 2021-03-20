4d434b8983968c1eb6fbb81c3cf7a7aca93f02d0
suvanbhooto <snbhooto@student.udm.ac.mu>
2021-03-21 01:03:23 +0400

    [addTwoInt.py] Deuxieme modification addTwoInt.py

--- addTwoInt.py
+++ addTwoInt.py
@@ -7,11 +7,15 @@ def add_num(a,b):
 def main():
 	import sys
 	print(sys.argv)
+	i=(len(sys.argv)-1)
+	print("le nombre d'arguments : ",i)
 	
-	x = int (sys.argv[1])
-	y = int (sys.argv[2])
-	
-	print(add_num(x,y))
+	if (i==2):
+		x = int (sys.argv[1])
+		y = int (sys.argv[2])
+		print(add_num(x,y))
+	else:
+		print("erreur : veuiller inserer que deux arguments")
 	
 main()
 		
